f6fe34b52bd78082b831900a893830fc8f4855d2
EL Ayachi Abdelmajid <93975585+EL-Ayachi-Abdelmajid@users.noreply.github.com>
2022-09-27 17:12:49 +0100

    Add : :+1: authantification for all users

--- src/Admin.java
+++ src/Admin.java
@@ -1,17 +1,29 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Admin {
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String password;
+public class Admin extends Person {
 
-    public String login(){
-        Scanner scanner = new Scanner(System.in);
-        email = scanner.nextLine();
-        password = scanner.nextLine();
-        return email;
+    static ArrayList<Person> admins = new ArrayList<Person>();
+    public Admin(String fName,String lName,String mail){
+        super(fName,lName,mail);
     }
+
+    public static ArrayList<Person> getAdmins(){
+        return admins;
+    }
+    public static void createAdmin(String fName,String lName,String mail){
+        admins.add(new Admin(fName,lName,mail));
+    }
+
+    public static void adminMenu(){
+        System.out.println("Select the next operation : Press");
+        System.out.println("1 : Create Former.");
+        System.out.println("2 : Create Learner.");
+        System.out.println("3 : Create promo.");
+    }
+
+
+
 
 
 }
--- src/Former.java
+++ src/Former.java
@@ -1,3 +1,15 @@
-public class Former {
+import java.util.ArrayList;
 
+public class Former extends Person {
+    static ArrayList<Person> formers = new ArrayList<Person>();
+    public Former(String fName,String lName,String mail){
+        super(fName,lName,mail);
+    }
+
+    public static ArrayList<Person> getFormers(){
+        return formers;
+    }
+    public static void createFormer(String fName,String lName,String mail){
+        formers.add(new Former(fName,lName,mail));
+    }
 }
