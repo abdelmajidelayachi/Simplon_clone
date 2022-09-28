fb9b0fb2c71878893129b4c86c21f425712369f2
EL Ayachi Abdelmajid <93975585+EL-Ayachi-Abdelmajid@users.noreply.github.com>
2022-09-28 15:30:27 +0100

    Create promo and assign former to it

--- src/Former.java
+++ src/Former.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Former extends Person {
     static ArrayList<Person> formers = new ArrayList<Person>();
     public Former(String fName,String lName,String mail){
-        super(fName,lName,mail);
+        super(fName,lName,mail,"FORMER",Former.getFormers().size()+1);
     }
 
     public static ArrayList<Person> getFormers(){
@@ -12,4 +12,6 @@ public class Former extends Person {
     public static void createFormer(String fName,String lName,String mail){
         formers.add(new Former(fName,lName,mail));
     }
+
+
 }
--- src/Promo.java
+++ src/Promo.java
@@ -1,2 +1,57 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Promo {
+
+    public String namePromo;
+    public String formerPromoRef;
+
+    static Scanner scanner = new Scanner(System.in);
+    static ArrayList<Promo> promos = new ArrayList<Promo>();
+    public Promo(String name,String formerRef){
+        this.namePromo = name;
+        this.formerPromoRef = formerRef;
+    }
+
+    // get all promo
+    public static ArrayList<Promo> getPromos(){
+        return promos;
+    }
+    public static boolean createPromo (){
+        System.out.println("------------------------------ Create Promotion ----------------------------");
+        System.out.print("Enter promotion Name : ");
+        String nPromo = scanner.nextLine();
+        System.out.println("Select reference of Former : ");
+        System.out.println("|-----------------------------------------------------------------------------------------|");
+        System.out.println("|   Select  |    Reference     |   First Name   |   Last Name    |           email        |");
+        System.out.println("|-----------------------------------------------------------------------------------------|");
+
+        ArrayList<Person> formers = Former.getFormers();
+        int i=0;
+        for (Person former : formers) {
+
+            System.out.println("|   Press " + i + "  |    " + former.reference + "     |   " + former.firstName + "   |   " + former.lastName + "    |" + former.email + "|");
+            System.out.println("|-----------------------------------------------------------------------------------------|");
+            i++;
+        }
+        String refScanner = scanner.nextLine();
+        int selectedNumRef = 0;
+        String referenceFormer = "";
+        try{
+            selectedNumRef = Integer.parseInt(refScanner);
+            referenceFormer = formers.get(selectedNumRef).reference;
+        }catch (Exception e){
+            System.out.println("Something went wrong " + e.getMessage());
+        }
+        if(!referenceFormer.isEmpty()){
+            promos.add(new Promo(nPromo,referenceFormer));
+            System.out.println("Promo created successfully 🙂");
+            return true;
+        }
+        return false;
+
+
+    }
+
+
 }
